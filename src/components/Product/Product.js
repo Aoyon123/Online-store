dbca3812c12553c2d46486888a45752fb074b2e4
Aoyon123 <49103044+Aoyon123@users.noreply.github.com>
2021-12-10 03:19:58 +0600

    Stock Information

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,11 +2,23 @@ import React from "react";
 import "./Product.css";
 
 const Product = (props) => {
-  const { name, img, price, stock } = props.product;
+  const { name, img, seller, price, stock } = props.product;
   return (
-    <div>
-      <img src={img} alt="" />
-      <h>{name}</h>
+    <div className="product">
+      <div>
+        <img src={img} alt="" />
+      </div>
+
+      <div>
+        <h4 className="product-name">{name}</h4>
+        <p>
+          <small>by:{seller}</small>
+        </p>
+        <p>Price:{price}</p>
+        <p>
+          <small>Only {stock} left in stock-Order soon!</small>
+        </p>
+      </div>
     </div>
   );
 };
