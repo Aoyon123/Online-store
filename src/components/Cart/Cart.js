11f581c08576aff93627ed608df00fda983562b5
Aoyon123 <49103044+Aoyon123@users.noreply.github.com>
2021-12-16 23:52:19 +0600

    Order

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./Cart.css";
 
 const Cart = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h3>Order Summary:</h3>
+    </div>
+  );
 };
 
 export default Cart;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
 import "./Shop.css";
 
@@ -30,8 +31,7 @@ const Shop = () => {
         ))}
       </div>
       <div className="cart-container">
-        <h3>Order Summary:</h3>
-        <h5>Items Order:{cart.length}</h5>
+        <Cart></Cart>
       </div>
     </div>
   );
